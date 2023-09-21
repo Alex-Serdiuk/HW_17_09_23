caa8a2dbc1576f473410de7d2467b9ea6a8f883c
Serdy_oj05@student.itstep.org <Serdy_oj05@student.itstep.org>
2023-09-21 21:13:12 +0300

    Add info

--- HW_17_09_23/Controllers/HomeController.cs
+++ HW_17_09_23/Controllers/HomeController.cs
@@ -10,41 +10,37 @@ public class HomeController : Controller
         // Заповнюємо дані для головної сторінки
         var model = new HomeModel
         {
-            PhotoPath = "/images/user-image.png",
-            AboutMe = "Тут ваша інформація про себе."
+            PhotoPath = "/images/user-image.jpg",
+            AboutMe = "Олексій Сердюк, Full-stack developer"
         };
         return View(model);
     }
     private static readonly List<Skill> allSkills = new List<Skill>
     {
-        new Skill { Name = "Навичка 1", Percentage = 80 },
-        new Skill { Name = "Навичка 2", Percentage = 70 },
+        new Skill { Name = "C#", Percentage = 90 },
+        new Skill { Name = "ASP.NET Core", Percentage = 60 },
+        new Skill { Name = "HTML", Percentage = 70 },
+        new Skill { Name = "CSS", Percentage = 60 },
+        new Skill { Name = "JavaScript", Percentage = 50 },
+        new Skill { Name = "C++", Percentage = 50 },
+        new Skill { Name = "Java", Percentage = 60 },
+        new Skill { Name = "Android App developing(Java)", Percentage = 50 },
+        new Skill { Name = "ADO.NET", Percentage = 80 },
+        new Skill { Name = "Windows Forms", Percentage = 90 },
+        new Skill { Name = "WPF", Percentage = 80 },
+        new Skill { Name = "SQL", Percentage = 80 },
+        new Skill { Name = "PHP", Percentage = 50 },
+        new Skill { Name = "Laravel", Percentage = 50 },
+        new Skill { Name = "Azure Cloud Services", Percentage = 50 },
         // Додайте інші навички
     };
     private readonly SkillsModel model = new SkillsModel
     {
-        FirstName = "Ваше прізвище, ім'я",
+        FirstName = "Олексій",
+        LastName = "Сердюк",
         Skills = allSkills,
     };
-    //public IActionResult Skills()
-    //{
-    //    List<Skill> filteredSkills;
-    //    string searchText = "";
-    //    if (string.IsNullOrWhiteSpace(searchText))
-    //    {
-    //        // Повернути всі навички, якщо пошуковий текст пустий
-    //        filteredSkills = model.Skills;
-    //    }
-    //    else
-    //    {
-    //        // Фільтрувати навички за введеним текстом
-    //        filteredSkills = model.Skills
-    //            .Where(skill => skill.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase))
-    //            .ToList();
-    //    }
 
-    //    return View(filteredSkills);
-    //}
     public IActionResult Skills(string searchText)
     {
         List<Skill> filteredSkills;
