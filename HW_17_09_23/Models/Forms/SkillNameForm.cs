92a40836aeeeb76edf567f32db77680fac4a9e80
Serdy_oj05@student.itstep.org <Serdy_oj05@student.itstep.org>
2023-10-08 14:57:01 +0300

    fix edit skillname

--- HW_17_09_23/Models/Forms/SkillNameForm.cs
+++ HW_17_09_23/Models/Forms/SkillNameForm.cs
@@ -7,7 +7,7 @@ namespace HW_17_09_23.Models.Forms
 		[Display(Name = "Name")]
 		public string Name { get; set; }
 
-		[Required(ErrorMessage = "Зображення обов'язкове")]
-		public IFormFile Image { get; set; }
+		//[Required(ErrorMessage = "Зображення обов'язкове")]
+		public IFormFile? Image { get; set; }
 	}
 }
